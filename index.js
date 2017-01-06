67d1e40165d516aa85d2b37a2119583a1968f48c
markosko <markosko.mm@gmail.com>
2017-01-07 00:38:57 +0100

    1.0.0

--- index.js
+++ index.js
@@ -7,12 +7,3 @@ var tnsModulesDir = path.join("node_modules", tnsPackage);
 
 var platform = process.env.PLATFORM;
 var platformOutDir = process.env.PLATFORM_DIR;
-/*
-exports.readPackageJson = resolver.readPackageJson;
-exports.getPackageMain = resolver.getPackageMain;
-
-exports.writePackageJson = function writePackageJson(dir, data) {
-    var packageJson = path.join(dir, "package.json");
-    fs.writeFileSync(packageJson, JSON.stringify(data, null, 4), 'utf8');
-};
-*/
